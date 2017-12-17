beb4610ce378deca4f112aba48607d31fdfccf38
David <davidr0141@gmail.com>
2017-12-17 23:38:26 +0200

    התחלתי את ה-ביאי ,אמשיך מחר בע"ה.

--- dotNet5778_Project01_6022_1395/BE/Nanny.cs
+++ dotNet5778_Project01_6022_1395/BE/Nanny.cs
@@ -8,5 +8,25 @@ namespace BE
 {
     public class Nanny
     {
+        int ID;
+        string lastName;
+        string firstName;
+        string DateOfBirth; //maybe not string? maybe Date?
+        int phone;
+        string adress;
+        bool elevator;
+        int floor;
+        int YearsOfExperience;
+        int maxChilds;
+        int minAgeOfChild;
+        int maxAgeOfChild;
+        bool HourlyRate;
+        double pricePerHour;
+        double pricePerMonth;
+        bool[] workdays;
+
+
+
+
     }
 }
